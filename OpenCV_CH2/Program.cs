1fb7132240f93f85aab143742243d2fb3b7aaab1
jihyukchoi <arrouw12@gmail.com>
2023-10-30 20:15:01 +0900

    132

--- OpenCV_CH2/Program.cs
+++ OpenCV_CH2/Program.cs
@@ -95,16 +95,78 @@ namespace OpenCV_CH2
             // track bar call back delegate 
             //TrackbarCallbackNative(int pos, IntPtr userData)
 
-            int value2 = 0;
+            //int value2 = 0;
 
-            Mat src2 = new Mat(new OpenCvSharp.Size(500, 500), MatType.CV_8UC3); // 크기 500x500 , 정밀도 8비트,usigned byte, 3채널 mat 객체 생성 
-            TrackbarCallbackNative trackbarCallback = new TrackbarCallbackNative(Event); // 트랙바 콜백함수에 Event 메서드 전달
+            //Mat src2 = new Mat(new OpenCvSharp.Size(500, 500), MatType.CV_8UC3); // 크기 500x500 , 정밀도 8비트,usigned byte, 3채널 mat 객체 생성 
+            //TrackbarCallbackNative trackbarCallback = new TrackbarCallbackNative(Event); // 트랙바 콜백함수에 Event 메서드 전달
 
-            Cv2.NamedWindow("Pallete");
-            Cv2.CreateTrackbar("Color", "Pallete", ref value2, 255, trackbarCallback, src2.CvPtr);
-            Cv2.WaitKey();
-            Cv2.DestroyAllWindows();  
+            //Cv2.NamedWindow("Pallete");
+            //Cv2.CreateTrackbar("Color", "Pallete", ref value2, 255, trackbarCallback, src2.CvPtr);
+            //Cv2.WaitKey();
+            //Cv2.DestroyAllWindows();  
 
+            //Mat img = new Mat(new OpenCvSharp.Size(640, 480), MatType.CV_8UC3);
+            //bool save;
+
+            //ImageEncodingParam[] prms = new ImageEncodingParam[] { new ImageEncodingParam(ImwriteFlags.JpegQuality, 100), new ImageEncodingParam(ImwriteFlags.JpegProgressive, 1) };
+            //save = Cv2.ImWrite(@"CV.jpeg", img, prms);
+            //Console.WriteLine(save);
+
+            //Cv2.VideoWriter(string fileName, FourCC fourcc, double fps, OpenCvSharp.Size frameSize, bool isColor = true); // 동영상 저장 함수
+
+            //VideoCapture capture = new VideoCapture(@"C:\Users\USER\Desktop\Star.mp4"); // 경로에 있는 동영상 객체
+            //Mat frame = new Mat(new OpenCvSharp.Size(capture.FrameWidth/2, capture.FrameHeight/2), MatType.CV_8UC3);// mat 객체
+            //VideoWriter videoWriter = new VideoWriter(); // VideoWriter 객체
+            //bool isWrite = false; //flag
+
+            //while (true)
+            //{
+            //    if (capture.PosFrames == capture.FrameCount)
+            //    {
+            //        capture.Open(@"C:\Users\USER\Desktop\Star.mp4");
+            //    }
+                
+            //    capture.Read(frame);
+                
+            //    Cv2.ImShow("VideoFrame", frame); 
+            //    int key = Cv2.WaitKey(33); //33밀리세컨 단위의 시간을 키입력 대기
+            //    if (key == 4) 
+            //    {
+            //        videoWriter.Open("Video.mp4", FourCC.XVID, 30, new OpenCvSharp.Size(frame.Width, frame.Height), true); //todo : avi형식으로는 저장이 예외가 발생하고, mp4로는 정상저장이 됨 
+            //        isWrite = true;
+            //    }
+            //    else if (key == 24)
+            //    {
+            //        videoWriter.Release();
+            //        isWrite = false;
+            //    }
+            //    else if (key == 'q')
+            //    {
+            //        break;
+            //    }
+
+            //    if (isWrite == true)
+            //    {
+            //        videoWriter.Write(frame);
+            //    }    
+            //}
+            
+            //videoWriter.Release();
+            //capture.Release();
+            //Cv2.DestroyAllWindows();
+
+            //Cv2.CvtColor(Mat src, Mat dst, ColorConversionCodes code, int dstCn = 0); // 색상 공간 변환 함수
+
+            Mat src = Cv2.ImRead(@"C:\Users\USER\Downloads\bird.jpg"); // 입력 이미지
+            Mat dst = new Mat(src.Size(), MatType.CV_8UC1); // 출력 이미지
+
+            Cv2.CvtColor(src, dst, ColorConversionCodes.BGR2GRAY); // 색상 공간 변환 함수
+
+            Cv2.ImShow("dst", dst); // show img 12
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+
+            // 원본 이미지 색상 공간2결과 이미지 색상 공간
         }
 
         private static void Event(int pos, IntPtr userdata) // callback 함수에 전달할 매개변수 함수
